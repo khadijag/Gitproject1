655c6308bc0746170defe92b262659bb3a545bcd
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-05-14 14:17:21 -0400

    new one commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //teamlead project
         System.out.println("I am Great1!!!");
         System.out.println("I am Great2!!!");
         System.out.println("I am Great3!!!");
@@ -11,5 +12,6 @@ public class Main {
 
         System.out.println("I am correct the code");
         System.out.println("I am not confused!");
+        System.out.println("hello my friends");
     }
 }
