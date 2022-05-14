1770b64a4afb8f626402bd68b86c9a1c23aacf94
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-05-14 12:35:54 -0400

    3rd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         System.out.println("I am Great1!!!");
         System.out.println("I am Great2!!!");
         System.out.println("I am Great3!!!");
-    }
 
+
+        System.out.println("I am correct the code");
+    }
 }
